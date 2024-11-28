53dc94748f93e1aeb082c21939cb6d90defd9ad2
Luís Felipe Vidotti <giovanisousa2601@gmail.com>
2024-11-28 15:49:06 -0300

    finalizando rotas de usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -7,6 +7,7 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const SECRET = process.env.SECRET;
+const auth = require("../auth/auth");
 
 //rota de criacao de conta
 router.post("/", (req,res) => {
@@ -47,6 +48,7 @@ router.post("/", (req,res) => {
     })
 })
 
+//rota de login
 router.post("/login", (req,res) => {
     if(!req.body.email || typeof req.body.email === undefined || req.body.email === null) {
         return res.status(400).json({message: "Erro, email invalido"});
@@ -72,6 +74,156 @@ router.post("/login", (req,res) => {
                 const token = jwt.sign({userId: usuario._id}, SECRET, {expiresIn: "1h"});
                 return res.status(200).json({message: "Login realizado com sucesso!!!", token:token});
             }
+        })
+    }).catch((erro) => {
+        return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+    })
+})
+
+//rota para ver todos os usuarios cadastrados (apenas admins)
+router.get("/", auth.verificaAdmin, (req,res) => {
+    Usuario.find().then((usuarios) => {
+        return res.status(200).json(usuarios);
+    }).catch((erro) => {
+        return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+    })
+})
+
+//rota para criar novas contas de admin (apenas admins)
+router.post("/admin", auth.verificaAdmin, (req,res) => {
+    if(!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+        return res.status(400).json({message: "Erro, nome invalido"});
+    }
+
+    if(!req.body.email || typeof req.body.email === undefined || req.body.email === null) {
+        return res.status(400).json({message: "Erro, email invalido"});
+    }
+
+    if(!req.body.senha || typeof req.body.senha === undefined || req.body.senha === null) {
+        return res.status(400).json({message: "Erro, senha invalida"});
+    }
+
+    //confirmacao de senha
+    if(req.body.senha2 !== req.body.senha) {
+        return res.status(400).json({message: "Erro, as senhas devem coincidir"});
+    }
+
+    if(req.body.senha.length < 4) {
+        return res.status(400).json({message: "Erro, senha muito curta"})
+    }
+
+    const salt = bcryptjs.genSaltSync(10);
+    const hash = bcryptjs.hashSync(req.body.senha, salt);
+
+    const novoAdmin = {
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: hash,
+        isAdmin: 1
+    }
+
+    new Usuario(novoAdmin).save().then((adminCriado) => {
+        return res.status(201).json({message: "Novo admin criado com sucesso!!!", adminCriado:adminCriado});
+    }).catch((erro) => {
+        return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+    })
+})
+
+//rota para alterar dados de outros usuarios (apenas admins)
+router.put("/admin/:id", auth.verificaAdmin, (req,res) => {
+    if(!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+        return res.status(400).json({message: "Erro, nome invalido"});
+    }
+
+    if(!req.body.email || typeof req.body.email === undefined || req.body.email === null) {
+        return res.status(400).json({message: "Erro, email invalido"});
+    }
+
+    if(!req.body.senha || typeof req.body.senha === undefined || req.body.senha === null) {
+        return res.status(400).json({message: "Erro, senha invalida"});
+    }
+
+    //confirmacao de senha
+    if(req.body.senha2 !== req.body.senha) {
+        return res.status(400).json({message: "Erro, as senhas devem coincidir"});
+    }
+
+    if(req.body.senha.length < 4) {
+        return res.status(400).json({message: "Erro, senha muito curta"})
+    }
+
+    Usuario.findOne({_id: req.params.id}).then((usuario) => {
+        if(!usuario) {
+            return res.status(404).json({message: "Erro, usuario a alterar nao encontrado"});
+        }
+
+        const salt = bcryptjs.genSaltSync(10);
+        const hash = bcryptjs.hashSync(req.body.senha, salt);
+
+        usuario.nome = req.body.nome;
+        usuario.email = req.body.email;
+        usuario.senha = hash;
+
+        usuario.save().then((usuarioAlterado) => {
+            return res.status(200).json({message: "Usuario alterado com sucesso!!!", usuarioAlterado: usuarioAlterado});
+        }).catch((erro) => {
+            return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+        })
+    }).catch((erro) => {
+        return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+    })
+})
+
+//rota para excluir usuarios (apenas admins)
+router.delete("/admin/:id", auth.verificaAdmin, (req,res) => {
+    Usuario.deleteOne({_id: req.params.id}).then(() => {
+        return res.status(200).json({message: "Usuario excluido com sucesso!!!"});
+    }).catch((erro) => {
+        return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
+    })
+})
+
+//rota para alterar os proprios dados
+router.put("/", auth.verificaUser, (req,res) => {
+    const user = req.user;
+
+    if(!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+        return res.status(400).json({message: "Erro, nome invalido"});
+    }
+
+    if(!req.body.email || typeof req.body.email === undefined || req.body.email === null) {
+        return res.status(400).json({message: "Erro, email invalido"});
+    }
+
+    if(!req.body.senha || typeof req.body.senha === undefined || req.body.senha === null) {
+        return res.status(400).json({message: "Erro, senha invalida"});
+    }
+
+    //confirmacao de senha
+    if(req.body.senha2 !== req.body.senha) {
+        return res.status(400).json({message: "Erro, as senhas devem coincidir"});
+    }
+
+    if(req.body.senha.length < 4) {
+        return res.status(400).json({message: "Erro, senha muito curta"})
+    }
+
+    Usuario.findOne({_id: user._id}).then((usuario) => {
+        if(!usuario) {
+            return res.status(404).json({message: "Erro, dados nao encontrados"});
+        }
+
+        const salt = bcryptjs.genSaltSync(10);
+        const hash = bcryptjs.hashSync(req.body.senha, salt);
+
+        usuario.nome = req.body.nome;
+        usuario.email = req.body.email;
+        usuario.senha = hash;
+
+        usuario.save().then((usuarioAlterado) => {
+            return res.status(200).json({message: "Dados alterados com sucesso!!!", usuarioAlterado:usuarioAlterado});
+        }).catch((erro) => {
+            return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
         })
     }).catch((erro) => {
         return res.status(500).json({errorMessage: "Erro interno no servidor, erro: "+erro})
